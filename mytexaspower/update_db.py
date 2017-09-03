4b7a5ad982c332b583ee89fff7ed8c36ffbfc5b1
Kris Field <kristopher.field@gmail.com>
2017-09-03 11:54:26 -0500

    testing and slack alert

--- mytexaspower/update_db.py
+++ mytexaspower/update_db.py
@@ -1,7 +1,9 @@
 import os
 import sqlite3
 import csv
+from slack_alert import slack_alert
 import urllib.request
+from urllib.error import URLError, HTTPError, ContentTooShortError
 
 class DatabaseManager(object):
   def __init__(self, db):
@@ -20,6 +22,9 @@ class DatabaseManager(object):
     self.conn.commit()
     return self.cur
 
+  def fetchall(self):
+    return self.cur.fetchall()
+
   def __del__(self):
     self.conn.close()
 
@@ -31,30 +36,42 @@ def update_db():
     csv_location = os.path.join(THIS_FOLDER, 'mypower.csv')
 
     url = 'http://www.powertochoose.org/en-us/Plan/ExportToCsv'
-    urllib.request.urlretrieve(url, csv_location)
+    try:
+        urllib.request.urlretrieve(url, csv_location)
+    except HTTPError as e:
+        print('The server couldn\'t fulfill the request.')
+        print('Error code: ', e.code)
+    except URLError as e:
+        print('Couldn\'t reach the server.')
+        print('Reason: ', e.reason)
+    except ContentTooShortError as e:
+        print('Download was interrupted.')
+    else:
+        #CONNECTING TO DB
+        db_location = os.path.join(THIS_FOLDER, 'mypower.db')
+        dbmgr = DatabaseManager(db_location)
+        dbmgr.query("DROP TABLE IF EXISTS offers")
+        dbmgr.query("DROP TABLE IF EXISTS user")
 
-    #CONNECTING TO DB
-    db_location = os.path.join(THIS_FOLDER, 'mypower.db')
-    dbmgr = DatabaseManager(db_location)
-    dbmgr.query("DROP TABLE IF EXISTS offers")
-    dbmgr.query("DROP TABLE IF EXISTS user")
+        #CREATING DB TABLE IF IT DOESN'T EXIST
+        dbmgr.query("CREATE TABLE IF NOT EXISTS offers ('idKey' INTEGER, 'TduCompanyName', 'RepCompany', 'Product', 'kwh500' INTEGER, 'kwh1000' INTEGER, 'kwh2000' INTEGER, 'FeesCredits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable' INTEGER, 'TermValue' INTEGER, 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits');")
 
-    #CREATING DB TABLE IF IT DOESN'T EXIST
-    dbmgr.query("CREATE TABLE IF NOT EXISTS offers ('idKey' INTEGER, 'TduCompanyName', 'RepCompany', 'Product', 'kwh500' INTEGER, 'kwh1000' INTEGER, 'kwh2000' INTEGER, 'FeesCredits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable' INTEGER, 'TermValue' INTEGER, 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits');")
+        #OPENING DOWNLOADED CSV TO SAVE IT INTO DB
+        with open(csv_location, 'rt') as fin:
+            imported_csv = csv.DictReader(fin) # comma is default delimiter
+            to_db = [(i['[idKey]'], i['[TduCompanyName]'], i['[RepCompany]'], i['[Product]'], i['[kwh500]'], i['[kwh1000]'], i['[kwh2000]'], i['[Fees/Credits]'], i['[PrePaid]'], i['[TimeOfUse]'], i['[Fixed]'], i['[RateType]'], i['[Renewable]'], i['[TermValue]'], i['[CancelFee]'], i['[Website]'], i['[SpecialTerms]'], i['[TermsURL]'], i['[Promotion]'], i['[PromotionDesc]'], i['[FactsURL]'], i['[EnrollURL]'], i['[PrepaidURL]'], i['[EnrollPhone]'], i['[NewCustomer]'], i['[MinUsageFeesCredits]']) for i in imported_csv]
 
-    #OPENING DOWNLOADED CSV TO SAVE IT INTO DB
-    with open(csv_location, 'rt') as fin:
-        imported_csv = csv.DictReader(fin) # comma is default delimiter
-        to_db = [(i['[idKey]'], i['[TduCompanyName]'], i['[RepCompany]'], i['[Product]'], i['[kwh500]'], i['[kwh1000]'], i['[kwh2000]'], i['[Fees/Credits]'], i['[PrePaid]'], i['[TimeOfUse]'], i['[Fixed]'], i['[RateType]'], i['[Renewable]'], i['[TermValue]'], i['[CancelFee]'], i['[Website]'], i['[SpecialTerms]'], i['[TermsURL]'], i['[Promotion]'], i['[PromotionDesc]'], i['[FactsURL]'], i['[EnrollURL]'], i['[PrepaidURL]'], i['[EnrollPhone]'], i['[NewCustomer]'], i['[MinUsageFeesCredits]']) for i in imported_csv]
+            #SAVING CSV INTO DB
+            dbmgr.querymany("INSERT INTO offers ('idKey', 'TduCompanyName', 'RepCompany', 'Product', 'kwh500', 'kwh1000', 'kwh2000', 'FeesCredits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable', 'TermValue', 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
 
-        #SAVING CSV INTO DB
-        dbmgr.querymany("INSERT INTO offers ('idKey', 'TduCompanyName', 'RepCompany', 'Product', 'kwh500', 'kwh1000', 'kwh2000', 'FeesCredits', 'PrePaid', 'TimeOfUse', 'Fixed', 'RateType', 'Renewable', 'TermValue', 'CancelFee', 'Website', 'SpecialTerms', 'TermsURL', 'Promotion', 'PromotionDesc', 'FactsURL', 'EnrollURL', 'PrepaidURL', 'EnrollPhone', 'NewCustomer', 'MinUsageFeesCredits') VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", to_db)
-
-      #DELETES LAST ROW
-        dbmgr.query('DELETE FROM offers WHERE "idKey"="END OF FILE";')
-    #COMMITING CHANGES AND CLOSING CONNECTION
-    del dbmgr
-    os.remove(csv_location)
-    print("Database updated")
+          #DELETES LAST ROW
+            dbmgr.query('DELETE FROM offers WHERE "idKey"="END OF FILE";')
+            dbmgr.query("SELECT * FROM offers")
+            total_rows = len(dbmgr.fetchall())
+        #COMMITING CHANGES AND CLOSING CONNECTION
+        del dbmgr
+        slack_alert("MyTexasPower Database was updated.\n *Total # Rows:* {}".format(total_rows))
+        os.remove(csv_location)
+        print("Database updated")
 
 update_db()
